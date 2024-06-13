8b80e266a67c315eb124c952f2362bf572aad507
Wishing-Qi <53641728+Wishing-Qi@users.noreply.github.com>
2024-06-13 20:43:24 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,21 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_liuqin-user
-add_lunch_combo omni_liuqin-userdebug
-add_lunch_combo omni_liuqin-eng
+
+if [ ! -d .repo/local_manifests ];
+then
+    mkdir -p .repo/local_manifests
+fi
+
+if [ ! -f .repo/local_manifests/local_liuqin.xml ];
+then
+    cp device/xiaomi/liuqin/local_liuqin.xml .repo/local_manifests/local_liuqin.xml
+fi
+
+if [ ! -d external/gflags ];
+then
+    repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all) external/gflags
+fi
+
+# For building with minimal manifest
+export ALLOW_MISSING_DEPENDENCIES=true
